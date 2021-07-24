fed239385419b8e9dc09e6e41d6ebd5c73b4b914
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 20:13:29 +0200

    Added documentation for ArrayModification.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/util/ArrayModification.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/util/ArrayModification.java
@@ -2,12 +2,32 @@ package com.denesgarda.Socketeer.util;
 
 import java.util.Arrays;
 
+/**
+ * Utilities used within the server to modify arrays
+ * @author denesgarda
+ */
 public class ArrayModification {
+
+    /**
+     * Append an element to the end of an array
+     * @param array The array the element should be added to
+     * @param value The element to be added
+     * @param <T> The type of array and element
+     * @return The new array
+     */
     public static<T> T[] append(T[] array, T value) {
         T[] result = Arrays.copyOf(array, array.length + 1);
         result[result.length - 1] = value;
         return result;
     }
+
+    /**
+     * Remove an element from an array
+     * @param array The array the element should be removed from
+     * @param value The element to be removed
+     * @param <T> The type of array and element
+     * @return The new array
+     */
     public static<T> T[] remove(T[] array, T value) {
         if (array == null) {
             return array;
