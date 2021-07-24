120afd17f41a89c4d4cb13bdcfa1ac8b2c206fcc
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:40:55 +0200

    Added documentation for ClientDisconnectedEvent.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/ClientDisconnectedEvent.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/ClientDisconnectedEvent.java
@@ -2,13 +2,28 @@ package com.denesgarda.Socketeer.data.event;
 
 import com.denesgarda.Socketeer.data.Connection;
 
+/**
+ * This is the event that's fired when a client disconnects from the server
+ * @author denesgarda
+ */
 public class ClientDisconnectedEvent extends Event {
+    /**
+     * The already closed connection that was used
+     */
     private final Connection connection;
 
+    /**
+     * The constructor of ClientDisconnectedEvent
+     * @param connection The connection that was used when the client was still connected to the server
+     */
     public ClientDisconnectedEvent(Connection connection) {
         this.connection = connection;
     }
 
+    /**
+     * Gets the connection
+     * @return The connection
+     */
     public Connection getConnection() {
         return this.connection;
     }
