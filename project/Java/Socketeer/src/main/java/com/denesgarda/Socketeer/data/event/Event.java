6ecbf701e1c125373194a16e2f94fa4c92628951
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:43:59 +0200

    Added documentation to Event.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/Event.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/Event.java
@@ -2,11 +2,23 @@ package com.denesgarda.Socketeer.data.event;
 
 import java.lang.reflect.Method;
 
+/**
+ * This is the superclass of all events
+ * @author denesgarda
+ */
 public class Event {
+    /**
+     * The constructor of Event
+     */
     public Event() {
 
     }
 
+    /**
+     * This is the method used within the server to call various events
+     * @param listener The listener to call the event to
+     * @param event The event to be called
+     */
     public static void callEvent(Listener listener, Event event) {
         for(Method method : listener.getClass().getMethods()) {
             if(method.isAnnotationPresent(EventHandler.class)) {
