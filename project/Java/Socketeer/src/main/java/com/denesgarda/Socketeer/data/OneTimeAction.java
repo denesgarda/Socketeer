90ec37d28cfcf5a75b8319211130a6aeeca9697a
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 19:58:44 +0200

    Added documentation for OneTimeAction.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/OneTimeAction.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/OneTimeAction.java
@@ -1,5 +1,15 @@
 package com.denesgarda.Socketeer.data;
 
+/**
+ * This is the class that manages one-time actions within connections
+ * @author denesgarda
+ */
 public interface OneTimeAction {
+
+    /**
+     * The action method that takes one Connection
+     * @param connection The Connection to be used within the action
+     * @throws Exception
+     */
     void action(Connection connection) throws Exception;
 }
