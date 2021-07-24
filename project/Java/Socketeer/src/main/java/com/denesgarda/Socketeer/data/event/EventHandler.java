c81193eec33d662f11a14b60625dccbdfd57a9c0
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:46:23 +0200

    Added documentation to EventHandler.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/EventHandler.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/EventHandler.java
@@ -3,6 +3,11 @@ package com.denesgarda.Socketeer.data.event;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
+/**
+ * This is the tag that should be used when declaring an event listener
+ * It is used to differentiate between normal methods and event listener methods
+ * @author denesgarda
+ */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface EventHandler {
 }
