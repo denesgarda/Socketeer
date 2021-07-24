109e1dd3928a49303dd53f73b2e285388ce78215
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:51:34 +0200

    Added documentation for ReceivedEvent.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/ReceivedEvent.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/ReceivedEvent.java
@@ -2,18 +2,43 @@ package com.denesgarda.Socketeer.data.event;
 
 import com.denesgarda.Socketeer.data.Connection;
 
+/**
+ * This is the event that's fired when the server receives on object from a client
+ * @author denesgarda
+ */
 public abstract class ReceivedEvent extends Event {
+    /**
+     * The connection used
+     */
     private final Connection connection;
 
+    /**
+     * The constructor of ReceivedEvent
+     * @param connection The connection through which the object was sent
+     */
     public ReceivedEvent(Connection connection) {
         this.connection = connection;
     }
 
+    /**
+     * Gets the connection
+     * @return The connection
+     */
     public Connection getConnection() {
         return this.connection;
     }
 
+    /**
+     * Read what the client sent
+     * @return The object read form the ObjectInputStream
+     * @throws Exception
+     */
     public abstract Object read() throws Exception;
 
+    /**
+     * Reply to the client
+     * @param object The object to be sent to the client through the ObjectOutputStream
+     * @throws Exception
+     */
     public abstract void reply(Object object) throws Exception;
 }
