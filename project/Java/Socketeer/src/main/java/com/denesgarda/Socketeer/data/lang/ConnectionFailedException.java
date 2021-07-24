d4161a212d77d1d29f453a07bbe279d307c0d145
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:56:13 +0200

    Added documentation for ConnectionFailedException.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/lang/ConnectionFailedException.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/lang/ConnectionFailedException.java
@@ -1,10 +1,21 @@
 package com.denesgarda.Socketeer.data.lang;
 
+/**
+ * This is the exception that's thrown when a client fails to connect to the server
+ */
 public class ConnectionFailedException extends RuntimeException {
+
+    /**
+     * The default constructor of ConnectionFailedException
+     */
     public ConnectionFailedException() {
         super("The connection failed for an unknown reason.");
     }
 
+    /**
+     * The secondary constructor of ConnectionFailedException
+     * @param messageFromServer The message that the server sent to the client with information about why the connection failed
+     */
     public ConnectionFailedException(String messageFromServer) {
         super("Message from server: " + messageFromServer);
     }
