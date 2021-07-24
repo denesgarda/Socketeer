7a2ed68b1b9b0a76d14e78085e32e561d49a8f07
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:37:12 +0200

    Added documentation to ClientConnectedEvent.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/ClientConnectedEvent.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/ClientConnectedEvent.java
@@ -2,19 +2,44 @@ package com.denesgarda.Socketeer.data.event;
 
 import com.denesgarda.Socketeer.data.Connection;
 
+/**
+ * This is the event that's fired when a client successfully establishes a connection with the server
+ * @author denesgarda
+ */
 public class ClientConnectedEvent extends Event {
+
+    /**
+     * The connection that is created
+     */
     private final Connection connection;
+
+    /**
+     * The type of connection the client used
+     */
     private final Connection.ConnectionType connectionType;
 
+    /**
+     * The constructor of ClientConnectedEvent
+     * @param connection The newly created connection between the server and client
+     * @param connectionType The type of connection the client used to connect to the server
+     */
     public ClientConnectedEvent(Connection connection, Connection.ConnectionType connectionType) {
         this.connection = connection;
         this.connectionType = connectionType;
     }
 
+    /**
+     * Gets the connection
+     * @return The connection
+     */
     public Connection getConnection() {
         return this.connection;
     }
 
+    /**
+     * Gets the connection type
+     * @return The connection type
+     */
     public Connection.ConnectionType getConnectionType() {
         return this.connectionType;
     }
