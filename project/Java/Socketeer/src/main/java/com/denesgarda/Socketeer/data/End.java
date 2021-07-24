1ec3f79d4fc20a39fc328983fa1e32fcc08adf46
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 17:15:47 +0200

    Added documentation for End.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/End.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/End.java
@@ -3,17 +3,36 @@ package com.denesgarda.Socketeer.data;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+/**
+ * The end of a connection; either a server or a client
+ * @author denesgarda
+ */
 public class End {
+    /**
+     * The address of the end
+     */
     private final String address;
 
+    /**
+     * The default constructor of End
+     * @throws UnknownHostException
+     */
     protected End() throws UnknownHostException {
         this.address = InetAddress.getLocalHost().getHostName();
     }
 
+    /**
+     * The secondary constructor of End
+     * @param address An overwritten address of the End
+     */
     protected End(String address) {
         this.address = address;
     }
 
+    /**
+     * Gets the address of the End
+     * @return The address of the End
+     */
     public String getAddress() {
         return address;
     }
