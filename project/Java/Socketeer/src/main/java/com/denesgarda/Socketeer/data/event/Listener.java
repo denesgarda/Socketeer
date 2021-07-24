a53ff4256e155ef9ff0e1390e2222d850ba87d02
Denes Garda <72540138+DenDen747@users.noreply.github.com>
2021-07-24 16:47:15 +0200

    Added documentation for Listener.java

--- project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/Listener.java
+++ project/Java/Socketeer/src/main/java/com/denesgarda/Socketeer/data/event/Listener.java
@@ -1,4 +1,8 @@
 package com.denesgarda.Socketeer.data.event;
 
+/**
+ * This is the interface that should be used for classes declared as event listeners
+ * @author denesgarda
+ */
 public interface Listener {
 }
